c82551f6737fc72da40c8d2c48231cbd18b4d0b3
juan.m.cintas.pena <juan.m.cintas.pena@accenture.com>
2018-02-27 11:16:16 +0100

    Servidor Zipkin

--- ServidorZipkin/src/main/java/es/ejercicio/microservicios/zipkin/ServidorZipkinApplication.java
+++ ServidorZipkin/src/main/java/es/ejercicio/microservicios/zipkin/ServidorZipkinApplication.java
@@ -2,10 +2,12 @@ package es.ejercicio.microservicios.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZipkinServer
 public class ServidorZipkinApplication {
 
