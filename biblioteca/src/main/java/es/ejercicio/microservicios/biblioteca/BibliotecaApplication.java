de4691d9d0a93ce923e809159f2813b7396859e0
juan.m.cintas.pena <juan.m.cintas.pena@accenture.com>
2018-03-01 17:23:56 +0100

    Servidor Hystrix

--- biblioteca/src/main/java/es/ejercicio/microservicios/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/es/ejercicio/microservicios/biblioteca/BibliotecaApplication.java
@@ -2,14 +2,14 @@ package es.ejercicio.microservicios.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
+@EnableCircuitBreaker
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
